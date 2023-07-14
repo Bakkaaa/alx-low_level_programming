c87084529bf692b837fab92fa1a7068df7b7a91a
Bakkaaa <136008893+Bakkaaa@users.noreply.github.com>
2023-07-14 02:22:43 +0200

    S

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-/* 
- * File: 4-puts.c 
- * Auth: Bakkaa
- */
-#include <stdio.h>  
- /*
-  * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
+#include <stdio.h> 
+  
+ /** 
+  * main - Prints "Programming is like building a multilingual 
+  *                puzzle, followed by a new line. 
+  * 
   * Return: Always 0. 
   */ 
  int main(void) 
  { 
          puts("\"Programming is like building a multilingual puzzle"); 
+  
          return (0); 
  }
